1959d65fcbc74bf6dfce511b0922c9b7214cc1c2
Dmitry Pakhilov <dmitrypakhilov@Dmitrys-Air.attlocal.net>
2023-07-20 00:26:21 -0700

    first 3 lessons

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,13 @@
 export function addBorder(picture: string[]): string[] {
-
-}
-
-// console.log(addBorder(["abc", "ded"]));
+    const wall = "*".repeat(picture[0].length + 2);
+  
+    picture.unshift(wall);
+    picture.push(wall);
+  
+    for (let i = 1; i < picture.length - 1; i++) {
+      picture[i] = "*".concat(picture[i], "*");
+    }
+  
+    return picture;
+  }
+  
